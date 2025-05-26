9a0c1f28871caa6b5c4e8ccb36766c762d0ecbe9
OrSlamBack <4ebyrekman@gmail.com>
2025-05-26 10:22:57 +0300

    Update white_5.cs

--- Lab7/Lab7/white_5.cs
+++ Lab7/Lab7/white_5.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace Lab_7
+namespace Lab_6
 {
     public class White_5
     {
@@ -42,8 +42,15 @@ namespace Lab_7
                 goals = _goals;
                 misses = _misses;
             }
+            public void Print()
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Голов : {Goals}");
+                Console.WriteLine($"Пропущено : {Misses}");
+                Console.WriteLine($"Очков : {Score}");
+            }
         };
-        public abstract class Team
+        public struct Team
         {
             private string name;
             private Match[] matches;
@@ -59,7 +66,11 @@ namespace Lab_7
             {
                 get
                 {
-                    if(matches==null)
+                    if(matches == null)
+                    {
+                        return 0;
+                    }
+                    if(matches.Length == 0)
                     {
                         return 0;
                     }
@@ -75,7 +86,11 @@ namespace Lab_7
             {
                 get
                 {
-                    if(matches == null)
+                    if (matches == null)
+                    {
+                        return 0;
+                    }
+                    if (matches.Length==0)
                     {
                         return 0;
                     }
@@ -91,7 +106,7 @@ namespace Lab_7
                 name = _name;
                 matches = new Match[0];
             }
-            public virtual void PlayMatch(int goals, int misses)
+            public void PlayMatch(int goals, int misses)
             {
                 if(matches == null)
                 {
@@ -107,7 +122,7 @@ namespace Lab_7
             }
             public static void SortTeams(Team[] array)
             {
-                if (array == null)
+                if(array == null)
                 {
                     return;
                 }
@@ -115,13 +130,13 @@ namespace Lab_7
                 {
                     for (int j = i; j - 1 >= 0; j--)
                     {
-                        if (array[j].TotalScore > array[j - 1].TotalScore)
+                        if (array[j].TotalScore > array[j-1].TotalScore)
                         {
-                            (array[j], array[j - 1]) = (array[j - 1], array[j]);
+                            (array[j], array[j-1]) = (array[j-1], array[j]);
                         }
-                        else if (array[j].TotalScore == array[j - 1].TotalScore && array[j].TotalDifference > array[j - 1].TotalDifference)
+                        else if(array[j].TotalScore == array[j-1].TotalScore && array[j].TotalDifference > array[j-1].TotalDifference)
                         {
-                            (array[j], array[j - 1]) = (array[j - 1], array[j]);
+                              (array[j], array[j-1]) = (array[j -1], array[j]);
                         }
                         else
                         {
@@ -142,97 +157,5 @@ namespace Lab_7
             }
         };
 
-        public class ManTeam : Team
-        {
-            private ManTeam derby;
-            public ManTeam Derby => derby;
-
-            public ManTeam(string _Name, ManTeam _Derby = null) : base(_Name) 
-            {
-                derby = _Derby;
-            }
-
-            public void PlayMatch(int goals, int misses, ManTeam team = null)
-            {
-                if (team == derby && team != null)
-                {
-                    goals++;
-                }
-                base.PlayMatch(goals, misses);
-            }
-            public void Print()
-            {
-                if (Name == null) return;
-                if (Matches == null) return;
-                Console.Write(Name);
-                Console.Write(" ");
-                Console.Write(TotalScore);
-                Console.Write(" ");
-                Console.WriteLine(TotalDifference);
-            }
-        }
-        public class WomanTeam : Team 
-        {
-            private int[] penalties;
-            public int[] Penalties
-            {
-                get
-                {
-                    if (penalties == null) return default(int[]);
-                    int[] _penalties = new int[penalties.Length];
-                    Array.Copy(penalties, _penalties, penalties.Length);
-                    // for (int i = 0; i < _marks.Length; i++) marks[i] = _marks[i];
-                    return _penalties;
-                }
-            }
-            public WomanTeam(string _Name) : base(_Name)
-            {
-                penalties =  new int[0];
-            }
-            public int TotalPenalties
-            {
-                get
-                {
-                    if(penalties == null)
-                    {
-                        return 0;
-                    }
-                    int sum = 0;
-                    for(int i = 0;i < penalties.Length; i++)
-                    {
-                        sum += penalties[i];
-                    }
-                    return sum;
-                }
-            }
-            public override void PlayMatch(int goals, int misses)
-            {
-                if (misses > goals)
-                {
-                    if(penalties == null)
-                    {
-                        penalties = new int[0];
-                    }
-                    int[] t = penalties;
-                    penalties = new int[t.Length + 1];
-                    for (int i = 0; i < t.Length; i++)
-                    {
-                        penalties[i] = t[i];
-                    }
-                    penalties[penalties.Length - 1] = misses-goals;
-                }
-                base.PlayMatch(goals, misses);
-            }
-            public void Print()
-            {
-                if (Name == null) return;
-                if (Matches == null) return;
-                Console.Write(Name);
-                Console.Write(" ");
-                Console.Write(TotalScore);
-                Console.Write(" ");
-                Console.WriteLine(TotalDifference);
-            }
-        }
     }
 }
